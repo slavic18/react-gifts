d48677e62834bc4f4cabcf492dff29214b3f0e1c
Kirill Borzov <oresh.work@gmail.com>
2017-02-23 13:00:32 +0200

    user model and controller added.

--- app/routers/users.js
+++ app/routers/users.js
@@ -1,8 +1,19 @@
 const jwt = require('jsonwebtoken');
+const config = require('../configs/config');
+const UsersSchema = require('../models/users');
+const UsersController = require('../controllers/users');
 
 module.exports = {
     beforeMiddleware: function(router) {
+        router.route('/user/register')
+            .post(function(req, res) {
+                UsersController.register(req, res);
+            })
 
+        router.route('/user/login')
+            .post(function(req, res) {
+                UsersController.login(req, res);
+            })
     },
     middleware: function(router) {
         router.use(function (req, res, next) {
@@ -10,11 +21,11 @@ module.exports = {
             var token = req.body.token || req.query.token || req.headers['x-access-token'];
             if (token) {
                 // verifies secret and checks exp
-                jwt.verify(token, appConfig.secretToken, function (err, decoded) {
+                jwt.verify(token, config.secretToken, function (err, decoded) {
                     if (err) {
                         return res.json({success: false, message: 'Failed to authenticate token.'});
                     } else {
-                        if (!decoded._doc) {
+                        if (!decoded) {
                             return res.status(403).send({
                                 success: false,
                                 message: 'Token is not valid'
@@ -33,7 +44,15 @@ module.exports = {
         });
     },
     afterMiddleware: function(router) {
+        router.route('/users')
+            .get(function(req, res) {
+                UsersController.get(req, res);
+            });
 
+        router.route('/user')
+            .put(function(req, res) {
+                UsersController.edit(req, res);
+            });
     }
 }
 
